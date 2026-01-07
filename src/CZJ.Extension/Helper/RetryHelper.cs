179ef38fa85505952f599627714e7c6b34e0bb14
陈兆杰 <15298506403@163.com>
2026-01-07 15:33:15 +0800

    修改重试

--- src/CZJ.Extension/Helper/RetryHelper.cs
+++ src/CZJ.Extension/Helper/RetryHelper.cs
@@ -8,28 +8,35 @@
         public static RetryHelper New => new();
 
         /// <summary>
-        /// 重试参数
+        /// 重试次数
         /// </summary>
         /// <param name="maxAttempts"></param>
         /// <returns></returns>
         public RetryHelper MaxAttempts(int maxAttempts)
         {
             _maxAttempts = maxAttempts;
-
             return this;
         }
 
+        /// <summary>
+        /// 重试延时时间
+        /// </summary>
+        /// <param name="delayMilliseconds"></param>
+        /// <returns></returns>
         public RetryHelper DelayMilliseconds(int delayMilliseconds)
         {
             _delayMilliseconds = delayMilliseconds;
-
             return this;
         }
 
         public void Execute(Action action)
         {
+            int currentAttempt = 0;
+            int remainingAttempts = _maxAttempts;
+
             while (true)
             {
+                currentAttempt++;
                 try
                 {
                     action();
@@ -37,7 +44,39 @@
                 }
                 catch
                 {
-                    if (_maxAttempts-- <= 0)
+                    if (remainingAttempts-- <= 0)
+                    {
+                        throw;
+                    }
+
+                    if (_delayMilliseconds > 0)
+                    {
+                        Thread.Sleep(_delayMilliseconds);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 带执行次数
+        /// </summary>
+        /// <param name="action"></param>
+        public void Execute(Action<int> action)
+        {
+            int currentAttempt = 0;
+            int remainingAttempts = _maxAttempts;
+
+            while (true)
+            {
+                currentAttempt++;
+                try
+                {
+                    action(currentAttempt);
+                    break;
+                }
+                catch
+                {
+                    if (remainingAttempts-- <= 0)
                     {
                         throw;
                     }
@@ -52,15 +91,19 @@
 
         public T Execute<T>(Func<T> func)
         {
+            int currentAttempt = 0;
+            int remainingAttempts = _maxAttempts;
+
             while (true)
             {
+                currentAttempt++;
                 try
                 {
                     return func();
                 }
                 catch
                 {
-                    if (_maxAttempts-- <= 0)
+                    if (remainingAttempts-- <= 0)
                     {
                         throw;
                     }
@@ -68,6 +111,160 @@
                     if (_delayMilliseconds > 0)
                     {
                         Thread.Sleep(_delayMilliseconds);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 带执行次数回调
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public T Execute<T>(Func<int, T> func)
+        {
+            int currentAttempt = 0;
+            int remainingAttempts = _maxAttempts;
+
+            while (true)
+            {
+                currentAttempt++;
+                try
+                {
+                    return func(currentAttempt);
+                }
+                catch
+                {
+                    if (remainingAttempts-- <= 0)
+                    {
+                        throw;
+                    }
+
+                    if (_delayMilliseconds > 0)
+                    {
+                        Thread.Sleep(_delayMilliseconds);
+                    }
+                }
+            }
+        }
+
+        public async Task ExecuteAsync(Func<Task> action)
+        {
+            int currentAttempt = 0;
+            int remainingAttempts = _maxAttempts;
+
+            while (true)
+            {
+                currentAttempt++;
+                try
+                {
+                    await action();
+                    break;
+                }
+                catch
+                {
+                    if (remainingAttempts-- <= 0)
+                    {
+                        throw;
+                    }
+
+                    if (_delayMilliseconds > 0)
+                    {
+                        await Task.Delay(_delayMilliseconds);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 带执行次数回调
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public async Task ExecuteAsync(Func<int, Task> action)
+        {
+            int currentAttempt = 0;
+            int remainingAttempts = _maxAttempts;
+
+            while (true)
+            {
+                currentAttempt++;
+                try
+                {
+                    await action(currentAttempt);
+                    break;
+                }
+                catch
+                {
+                    if (remainingAttempts-- <= 0)
+                    {
+                        throw;
+                    }
+
+                    if (_delayMilliseconds > 0)
+                    {
+                        await Task.Delay(_delayMilliseconds);
+                    }
+                }
+            }
+        }
+
+        public async Task<T> ExecuteAsync<T>(Func<Task<T>> func)
+        {
+            int currentAttempt = 0;
+            int remainingAttempts = _maxAttempts;
+
+            while (true)
+            {
+                currentAttempt++;
+                try
+                {
+                    return await func();
+                }
+                catch
+                {
+                    if (remainingAttempts-- <= 0)
+                    {
+                        throw;
+                    }
+
+                    if (_delayMilliseconds > 0)
+                    {
+                        await Task.Delay(_delayMilliseconds);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 带执行次数回调
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public async Task<T> ExecuteAsync<T>(Func<int, Task<T>> func)
+        {
+            int currentAttempt = 0;
+            int remainingAttempts = _maxAttempts;
+
+            while (true)
+            {
+                currentAttempt++;
+                try
+                {
+                    return await func(currentAttempt);
+                }
+                catch
+                {
+                    if (remainingAttempts-- <= 0)
+                    {
+                        throw;
+                    }
+
+                    if (_delayMilliseconds > 0)
+                    {
+                        await Task.Delay(_delayMilliseconds);
                     }
                 }
             }
---
+++ src/TestProject/RetryTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    [TestClass]
+    public sealed class RetryTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            RetryHelper.New
+             .MaxAttempts(3)
+             .DelayMilliseconds(1000 * 5).Execute((index) =>
+             {
+                 Console.WriteLine($"第{index}次重试");
+                 throw new Exception("测试异常");
+             });
+        }
+    }
+}
