aaeab1ab4890d9149bdf5e8cbac5913f3a7992d0
陈兆杰 <15298506403@163.com>
2026-01-07 10:01:45 +0800

    commit

---
+++ src/TestProject/AppConfigTest.cs
@@ -0,0 +1,23 @@
+﻿using CZJ.Extension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    [TestClass]
+    public sealed class AppConfigTest
+    {
+        [TestMethod]
+        public void TestAppConfigSettings()
+        {
+            var appConfig = AppConfigHelper.OpenConfig(Path.Combine(Common.ApplicationBaseDirectory, "Configs/App.config"));
+            var flag = appConfig.TryGet<bool>("Exist", out bool value);
+            Assert.IsTrue(flag);
+            flag = appConfig.TryGet("Language", out string language);
+            Assert.IsTrue(flag);
+        }
+    }
+}
